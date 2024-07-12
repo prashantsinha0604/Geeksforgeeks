5a91b9b19a52714981ecd44e04e6a214e8264195
prashantsinha0604 <157583638+prashantsinha0604@users.noreply.github.com>
2024-07-12 14:03:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+}
+Node* removeDuplicates(Node *root);
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		Node *head = NULL;
+        Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+//Function to remove duplicates from sorted linked list.
+Node *removeDuplicates(Node *head)
+{
+ // your code goes here
+    Node * curr = head -> next , * prev = head  ;
+    while( curr )
+    {
+        if( prev -> data == curr -> data )
+        {
+            curr = curr -> next ;
+        }
+        else 
+        {
+            prev -> next = curr ;
+            prev = curr ;
+            curr = curr -> next ;
+        }
+    }
+    
+    if(curr and prev -> next == nullptr )
+    {
+        return head;
+    }
+    else
+    {
+        prev -> next = nullptr;
+    }
+    
+    return head;
+    
+}
